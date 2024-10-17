e09af02dbff5e810739eacfe1a64e3f2f6903f9b
soumo <soumo@kali>
2024-10-17 19:20:11 +0530

    fix(cloudinary):image didnot uploading as the cloudinary object didnt congigure properly

--- packages/user-services/src/utils/imageUpload.ts
+++ packages/user-services/src/utils/imageUpload.ts
@@ -1,19 +1,24 @@
 import { v2 as cloudinary } from 'cloudinary';
 import { generateUUID } from './uuid';
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 const uploadImage = (image: string) => {
-    const id = generateUUID();
-    return new Promise((resolve, reject) => {
-        cloudinary.uploader.upload(
-            image,
-            { public_id: id },
-            function (error, result) {
-                if (result && result.secure_url)
-                    return resolve(result.secure_url);
-                return reject(error);
-            }
-        );
-    });
+  const id = generateUUID();
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload(
+      image,
+      { public_id: id },
+      function (error, result) {
+        if (result && result.secure_url) return resolve(result.secure_url);
+        return reject(error);
+      }
+    );
+  });
 };
 
 export default uploadImage;
