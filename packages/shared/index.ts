25caddb3f13f17b2a795fffbe380ff9b2f7c6aa2
soumo <dsoumitra693@gmail.com>
2024-11-18 20:03:51 +0530

    chore: add Docker Compose configuration for service orchestration
    - Added  to define and orchestrate services for the chat app.
    - Configured Zookeeper and Kafka for message brokering.
    - Added , , , and  with Docker build contexts and environment settings.
    - Defined  for seamless inter-service communication.
    - Streamlined service dependencies to ensure proper startup order.

--- packages/shared/index.ts
+++
@@ -1,4 +0,0 @@
-export { asyncErrorHandler } from './utils/asyncErrorHandler';
-export { generateUUID } from './utils/uuid';
-export { init_db } from './db';
-export { account, userContacts, userSettings, users } from './db/schema';
